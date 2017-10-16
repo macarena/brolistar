5c4c907108fa3a10a4ce7de724b1a30488952275
CaioLibSalf <csalfatis@gmail.com>
2017-10-16 19:17:21 -0200

    mapa 2
    adicionado lava

--- mapa.py
+++ mapa.py
@@ -2,20 +2,22 @@ from random import randint
 import csv
 
 class Quadrado:
-    tipos = ["agua", "grama", "areia", "estrada", "pedra", "arbusto"]
+    tipos = ["agua", "grama", "areia", "estrada", "pedra", "arbusto", "lava"]
     amarelo = color(255, 230, 0)
     azul_escuro = color(7, 0, 255)
     marrom_escuro = color(85, 25, 27)
     verde_claro = color(10, 237, 7)
     marrom_claro = color(193, 109, 111)
     verde_escuro = color(48, 72, 36)
+    vermelho = color(255,0,0)
     cores = {
         "agua": azul_escuro,
         "pedra": marrom_escuro,
         "areia": amarelo,
         "grama": verde_claro,
         "estrada": marrom_claro,
-        "arbusto": verde_escuro
+        "arbusto": verde_escuro,
+        "lava": vermelho
     }
     
     def __init__(self, tipo, linha, coluna):
@@ -37,7 +39,7 @@ class Mapa:
         self.altura = altura
         self.escala = 40
         #self.criarQuadrados()
-        self.importarQuadrados("data/mapa1.csv")
+        self.importarQuadrados(r"data\mapa2.csv")
         
     def criarQuadrados(self):
         for linha in range(self.altura):
